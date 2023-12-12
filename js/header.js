cff2cdba2808cb97c474d1a70bed91ddabffdd22
JiHye0214 <qkrwlgp1526@gmail.com>
2023-12-12 20:24:29 +0900

    modify from jh

--- js/header.js
+++ js/header.js
@@ -1,37 +1,61 @@
-const $menuWrap = document.querySelector(".menuWrap")
-const $menuBtnWrap = document.querySelector(".menuBtnWrap");
+const textColor = ["#63da53", "#ff748b", "#ffc400", "#dbdbdb"]
+const menuArr = document.querySelectorAll(".menuText")
+const $menuWrap = document.querySelector("menuTitleWrap")
 
-const menuTitleArr = document.querySelectorAll(".menuTitleWrap > .menuText");
-const menuBtnArr = document.querySelectorAll(".menuBtnWrap > .menuText");
+const toggleArr = document.querySelectorAll(".raiseToggle")
+const $raiseBtn = document.querySelector(".raiseBtn")
 
-// 메뉴 축소
-const menuUp = () => {
-    $menuBtnWrap.classList.remove('slideMenuDown');
-    $menuBtnWrap.classList.add('slideMenuUp');
-};
-
-// 메뉴 확장
-const menuDown = () => {
-    $menuBtnWrap.classList.remove('slideMenuUp');
-    $menuBtnWrap.classList.add('slideMenuDown');
-};
-
-// 화면 이동 
-const clickMenuBtn = (index) => {
-    for(let i=0; i<3; i++){
+// 메뉴
+const clickMenu = (index) => {
+    for(let i=0; i<menuArr.length; i++){
         if(i == index){
-            menuTitleArr[i].style.display = `block`;
+            menuArr[i].style.color = textColor[i];
         } else {
-            menuTitleArr[i].style.display = `none`;
+            menuArr[i].style.color = textColor[3]
         }
     }
 }
 
-$menuWrap.addEventListener("mouseout", menuUp);
-$menuWrap.addEventListener("mouseover", menuDown);
+const touchMenu = (index) => {
+    for(let i=0; i<menuArr.length; i++){
+        if(i == index){
+            menuArr[i].style.color = textColor[i];
+        } else {
+            menuArr[i].style.color = textColor[3]
+        }
+    }
+}
+
 
 for(let i=0; i<3; i++){
-    menuBtnArr[i].onclick = function() {
-        clickMenuBtn(i);
+    menuArr[i].onmouseover = function() {
+        touchMenu(i);
+    }
+    menuArr[i].onclick = function() {
+        clickMenu(i);
+    }
+}
+
+// 같이 키우키 메뉴 눌렀을 때
+const clickRaiseBtn = () => {
+    toggleArr[0].style.display = `block`
+}
+
+$raiseBtn.addEventListener("click", clickRaiseBtn);
+
+// 토글 제어
+const clickToggle = (index) => {
+    for(let i=0; i<toggleArr.length; i++){
+        if(i == index){
+            toggleArr[i].style.display = `none`;
+        } else {
+            toggleArr[i].style.display = `block`;
+        }
+    }
+}
+
+for(let i=0; i<2; i++){
+    toggleArr[i].onclick = function() {
+        clickToggle(i);
     }
 }
