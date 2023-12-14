d3385dff954ae59deae6bc31dcf918904be27879
JiHye0214 <142701386+JiHye0214@users.noreply.github.com>
2023-12-14 20:23:14 +0900

    Delete myChildFront directory

--- myChildFront/js/modal.js
+++
@@ -1,9 +0,0 @@
-const $dropUserClose = document.querySelector(".dropUserCloseBtn");
-const $modal = document.querySelector(".modal");
-
-const clickDropUserCloseBtn = () => {
-    $modal.style.visibility = `hidden`;
-    $body.style.overflow = "unset";
-};
-
-$dropUserClose.addEventListener("click", clickDropUserCloseBtn);
